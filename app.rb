8c669463c2829cb26cb32eba799e6219b5b6783a
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-10-26 15:34:48 +0100

    created random routes and erb file

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  erb(:index)
+end
+
+get '/secret' do
+  erb(:index)
+end
+
+get '/moreroutes' do
+  erb(:index)
+end
+
+get '/routesagain' do
+  erb(:index)
+end
+
+get '/cat' do
+  
+end
